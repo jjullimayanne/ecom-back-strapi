5ac3fef7309072fe8537cdb23ca63d04648eb10a
Julli Mayanne <jullimayanne9@gmail.com>
2024-11-04 21:58:07 -0300

    feat: add configs

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+  settings: {
+    cache: {
+      enabled: true,
+      models: [
+        { model: "user", maxAge: 2592000000 },
+        { model: "product", maxAge: 2592000000 },
+      ],
+      type: "memory",
+    },
+  },
+};
