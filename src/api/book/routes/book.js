41acac0772b5f2ec6217270196afdece986c4618
Julli Mayanne <jullimayanne9@gmail.com>
2024-11-24 22:22:26 -0300

    feature: add new contents

--- src/api/book/routes/book.js
+++ src/api/book/routes/book.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * product router
+ * book router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::product.product');
+module.exports = createCoreRouter('api::book.book');
